6e22648eb4a6ffdbd5cc993218496d35fc1ef795
Kelly Sheddy <jungaka07@gmail.com>
2018-11-27 19:22:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+class Shoe
+  
+  attr_accessor :brand, :color, :genre
+  
+  def initialize(title)
+    @title = title
+  end 
+  
+  def title
+    @title
+  end 
+
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
